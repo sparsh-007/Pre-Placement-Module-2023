8e7ca8ea5c96561272b937870e501a923c7bcb6d
Sparsh Kapoor <93381005+sparsh-007@users.noreply.github.com>
2022-07-07 21:44:01 +0530

    rotate array

---
+++ ALGO I/Day 2/rotate_array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        int res[]=new int[n];
+        for(int i=0;i<n;i++){
+            res[(i+k)%n]=nums[i];
+        }
+ for(int i=0;i<n;i++){
+     nums[i]=res[i];
+ }
+    }
+}
