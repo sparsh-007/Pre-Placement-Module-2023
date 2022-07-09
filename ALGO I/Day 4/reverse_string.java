5aa7e748861552bd35dcd5209ee4dc84f63b92b7
Sparsh Kapoor <93381005+sparsh-007@users.noreply.github.com>
2022-07-10 01:03:03 +0530

    Update reverse_string.java

--- ALGO I/Day 4/reverse_string.java
+++ ALGO I/Day 4/reverse_string.java
@@ -1,1 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        char temp;
+       int start=0,end=s.length-1;
+        while(start<end){
+            temp=s[start];
+            s[start]=s[end];
+            s[end]=temp;
+            start++;
+            end--;
+        }
+        
 
+    }
+}
